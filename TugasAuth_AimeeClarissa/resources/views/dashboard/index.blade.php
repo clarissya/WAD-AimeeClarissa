ff95a30ab9b2d3146c0fb0eb61c1b034330ee405
clarissya <ad28ya@gmail.com>
2025-04-22 23:25:32 +0700

    Update index.blade.php

--- TugasAuth_AimeeClarissa/resources/views/dashboard/index.blade.php
+++ TugasAuth_AimeeClarissa/resources/views/dashboard/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Dashboard Bunga</title>
+  <title>Mystical Creature Dashboard</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
